727b746ecece9a6b332cbf3625b200c4e1aa5330
Garethgareth26 <114641031+Garethgareth26@users.noreply.github.com>
2022-10-11 12:12:21 +0700

    Add files via upload

---
+++ tugas 1 praktikum (nomor 1).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main () {
+
+   string npm = "2210631170071";
+   string nama = "Gareth Mu'ammar Dysa";
+   int umur = 18;
+   string kelas = "1D";
+   string hobi = "memasak dan olahraga";
+   string citacita = "front end developer";
+
+   cout << "npm:" << npm << endl;
+   cout << "nama:" << nama << endl;
+   cout << "umur:" << umur << endl;
+   cout << "kelas:" << kelas << endl;
+   cout << "hobi:" << hobi << endl;
+   cout << "citacita:" << citacita << endl;
+
+
+return 0;
+
+}
+
